8ba6a70c30085bd911577b06dee3a2e15ea0f00c
Peeter Stamberg <peeterstamberg@gmail.com>
2016-10-13 16:27:20 +0300

    mitte vajalikud eemaldatud

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String args[]){
-        System.out.println("Projekt: faktileht algus");
-    	System.out.println("autor:Peeter");
-        System.out.println("aasta: 2016");
-    }
-
-}
